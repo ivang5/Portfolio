1f3a8f3631be31b015fea23a570b2bc6fcae29de
ivang5 <ivanprogaborov@gmail.com>
2024-09-15 23:20:21 +0200

    Testing intersection observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,176 @@
+import { useInView } from "react-intersection-observer";
+
 function App() {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+    // delay: 1000,
+  });
+
   return (
     <div>
-      <h1 className="text-4xl text-blue-500">Portfolio</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <div ref={ref}>
+        <h1
+          className={`text-4xl text-blue-500 delay-300 transition-all duration-500 text-center
+            ${!inView && "opacity-0 -translate-x-10 blur-sm"} 
+            ${inView && "opacity-100 translate-x-0 blur-none"}`}
+        >{`Header inside viewport ${inView}.`}</h1>
+      </div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        fuga officiis ab nostrum quia quo quis, ut ipsam similique debitis unde
+        obcaecati! Officia sequi omnis dolore voluptatem doloribus iusto ullam.
+      </p>
     </div>
   );
 }
